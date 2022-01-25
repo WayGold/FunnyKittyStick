0154a256849d04aa779045c0f5a9a26d7cb1ad34
Wei Zeng <wzeng@ucsd.edu>
2022-01-25 11:20:31 -0700

    Revert "AIService Update:"
    This reverts commit 5c00fcfeb89e84dfd476d109021ab9353e6d7574.

--- Assets/Scripts/aiService/agent.cs
+++ Assets/Scripts/aiService/agent.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using MovementOutputs;
 using static AIService.aiService;
 
@@ -11,57 +10,23 @@ public class agent : MonoBehaviour
     public Rigidbody targetRB;
     public float maxSpeed;
     public float maxRotationSpeed;
-    public float idleRadius;
-    public float maxTargetSpeed;
 
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        KinematicSteeringOutput currentMovement;
-
-        // Set a radius around the agent determining an idle range
-        // Only head track + orientation matching 
-        if (isInRadius(agentRB.position, targetRB.position, idleRadius))
-        {
-            currentMovement.linearVelocity = new Vector3(0, 0, 0);
-            currentMovement.rotVelocity = 0;
-        }
-        else
-        { currentMovement = kinematicSeek(agentRB, targetRB, maxSpeed); }
+        KinematicSteeringOutput currentMovement = kinematicSeek(agentRB, targetRB, maxSpeed);
 
         // Fix y so the agent don't fly
         currentMovement.linearVelocity.y = 0;
-
-        if (!isTooFast(targetRB, maxTargetSpeed))
-        {
-            Debug.Log("Not Too Fast With Target Velocity - " + targetRB.velocity);
-            agentRB.velocity = currentMovement.linearVelocity;
-        }
-        {
-            Debug.Log("Too Fast!");
-        }
-           
-
+        agentRB.velocity = currentMovement.linearVelocity;
 
         Quaternion deltaRot = Quaternion.Euler(new Vector3(0, currentMovement.rotVelocity * Mathf.Rad2Deg, 0));
         agentRB.MoveRotation(deltaRot);
-    }
-
-    bool isInRadius(Vector3 agent, Vector3 target, float radius)
-    {
-        if ((target.x - agent.x) * (target.x - agent.x) + (target.z - agent.z) * (target.z - agent.z) <= radius)
-            return true;
-
-        return false;
-    }
-
-    bool isTooFast(Rigidbody target, float maxTargetSpeed)
-    {
-        if (target.velocity.magnitude > maxTargetSpeed)
-            return true;
-        return false;
     }
 }

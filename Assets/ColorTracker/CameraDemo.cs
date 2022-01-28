f8a4ed9066142fa80be13087a4e4ead68f84bed6
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2022-01-28 14:21:14 -0700

    two test scenes, added color tracking to both scenes one with all the objects one with only the tracking objects. Needs to be optimized it is lagging with all the other processes in the scene.

---
+++ Assets/ColorTracker/CameraDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDemo : MonoBehaviour
+{
+
+    public static WebCamTexture cameraTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameraTexture == null)
+            cameraTexture = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = cameraTexture;
+
+        if (!cameraTexture.isPlaying)
+            cameraTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

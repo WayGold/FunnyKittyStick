c71bc3632d99cd107d9c6bc963f92216026abfdd
Han <57443614+currybeefer@users.noreply.github.com>
2022-01-30 12:03:25 -0700

    TEST

--- Assets/ColorTracker/CameraDemo.cs
+++ Assets/ColorTracker/CameraDemo.cs
@@ -8,17 +8,36 @@ public class CameraDemo : MonoBehaviour
     public static WebCamTexture cameraTexture;
 
     // Start is called before the first frame update
-    void Start()
+    //void Start()
+    //{
+    //    if (cameraTexture == null)
+    //        cameraTexture = new WebCamTexture();
+
+    //    GetComponent<Renderer>().material.mainTexture = cameraTexture;
+
+    //    if (!cameraTexture.isPlaying)
+    //        cameraTexture.Play();
+    //}
+
+    public string deviceName;
+    public WebCamTexture tex;
+    // Use this for initialization  
+    IEnumerator Start()
     {
-        if (cameraTexture == null)
-            cameraTexture = new WebCamTexture();
-
-        GetComponent<Renderer>().material.mainTexture = cameraTexture;
-
-        if (!cameraTexture.isPlaying)
-            cameraTexture.Play();
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            deviceName = devices[0].name;
+            tex = new WebCamTexture(deviceName);
+            
+            gameObject.GetComponent<Renderer>().material.mainTexture = tex;
+            tex.Play();
+        }
+        else
+        {
+        }
     }
-
     // Update is called once per frame
     void Update()
     {

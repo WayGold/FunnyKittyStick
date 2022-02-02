262cd8de7d8c999b88b18a6c5215d449fba384d4
Inutto <57230511+Inutto@users.noreply.github.com>
2022-02-02 15:36:08 -0700

    Add more collider and disable the global music

--- Assets/Scripts/TempGameManager.cs
+++ Assets/Scripts/TempGameManager.cs
@@ -34,7 +34,7 @@ public class TempGameManager : MonoBehaviour
             }
         }
 
-        DontDestroyOnLoad(gameObject);
+        // DontDestroyOnLoad(gameObject);
     }
 
     #endregion SINGLETON
@@ -44,8 +44,8 @@ public class TempGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InitializeAudioData();
-        CheckPlayBGM();
+       //  InitializeAudioData();
+       //  CheckPlayBGM();
     }
 
     // Update is called once per frame

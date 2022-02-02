1a0907800bac862e648cf98f7942cf36f1e4d78d
Inutto <57230511+Inutto@users.noreply.github.com>
2022-02-02 09:30:11 -0700

    Update buildSetting,UIAnimation Implementation, and test RestartGame

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

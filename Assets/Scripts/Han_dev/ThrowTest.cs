a41fa727c7ec1398c58e08620f1552dfa7f53c01
Han <57443614+currybeefer@users.noreply.github.com>
2022-02-22 00:46:32 -0700

    add down throw

--- Assets/Scripts/Han_dev/ThrowTest.cs
+++ Assets/Scripts/Han_dev/ThrowTest.cs
@@ -28,6 +28,15 @@ public class ThrowTest : MonoBehaviour
 
         TryEndThrow();
     }
+    void TryEndThrow()
+    {
+        if (isTrowing == true && cat.transform.position.x >= targetPos.x - 1f && cat.transform.position.x <= targetPos.x + 1f
+    && cat.transform.position.z >= targetPos.z - 1f && cat.transform.position.z <= targetPos.z + 1f)
+        {
+            cat.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            isTrowing = false;
+        }
+    }
     void ThrowCat()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -41,37 +50,53 @@ public class ThrowTest : MonoBehaviour
 
             Vector3 jumpVelpcity = CaculateTrowVelocity(hit.point);
             cat.GetComponent<Rigidbody>().velocity = jumpVelpcity;
+
             targetPos = hit.point;
             isTrowing = true;
-        }
-    }
-    void TryEndThrow()
-    {
-        if (isTrowing == true && cat.transform.position.x >= targetPos.x - 0.5f && cat.transform.position.x <= targetPos.x + 0.5f
-    && cat.transform.position.z >= targetPos.z - 0.5f && cat.transform.position.z <= targetPos.z + 0.5f)
-        {
-            cat.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            isTrowing = false;
+            
+            print("startPos:" + cat.transform.position);
+            print("TargetPos: " + targetPos);
         }
     }
     Vector3 CaculateTrowVelocity(Vector3 targetPos)
     {
         Vector3 catSize = cat.GetComponent<Collider>().bounds.size;
+
         float h = targetPos.y - (cat.transform.position.y-catSize.y/2)+addHeight;
         float s = Vector2.Distance(new Vector2(targetPos.x, targetPos.z),
             new Vector2(cat.transform.position.x, cat.transform.position.z));
-        print("s="+ s);
+        print("s="+ s+" h= "+h);
 
+        return (h>=0)?UpThrow(h,s):DownThrow(h, s);
+    }
+    Vector3 UpThrow(float h,float s)
+    {
         float v0 = Mathf.Sqrt(2 * G * h);
         float v1 = s / (Mathf.Sqrt((2 / G) * v0 - 2 * h / G));
 
         //horizontal velocity
-        Vector3 vs = new Vector3(targetPos.x-cat.transform.position.x, 
-                                    cat.transform.position.y, 
-                                    targetPos.z-cat.transform.position.z).normalized * v1;
+        Vector3 vs = new Vector3(targetPos.x - cat.transform.position.x,
+                                    cat.transform.position.y,
+                                    targetPos.z - cat.transform.position.z).normalized * v1;
         //vertical velocity
-        Vector3 vh = new Vector3(0, targetPos.y - cat.transform.position.y, 0).normalized*v0;
+        Vector3 vh = new Vector3(0, targetPos.y - cat.transform.position.y, 0).normalized * v0;
 
+        print("UpThrow:"+"vh= " + vh + " vs=" + vs);
         return vh + vs;
+    }
+    Vector3 DownThrow(float h, float s)
+    {
+        float v0 = 0;
+        float v1 = s / (Mathf.Sqrt((-2*h)/G));
+
+        //horizontal velocity
+        Vector3 vs = new Vector3(targetPos.x - cat.transform.position.x,
+                                   cat.transform.position.y,
+                                    targetPos.z - cat.transform.position.z).normalized * v1;
+        //vertical velocity
+        Vector3 vh = new Vector3(0, targetPos.y - cat.transform.position.y, 0).normalized * v0;
+
+        print("DownThrow:"+"vh = " + vh + " vs=" + vs);
+        return vs;
     }
 }

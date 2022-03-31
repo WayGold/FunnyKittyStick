42505be692d3451e650c1f095ce015fe5cea3ad0
Wei Zeng <wzeng@ucsd.edu>
2022-03-31 12:20:41 -0600

    Cat selector

--- Assets/Scripts/Wei_dev/CatSelector.cs
+++ Assets/Scripts/Wei_dev/CatSelector.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class CatSelector : MonoBehaviour
 {
-    [SerializeField] GameObject cat_1;
-    [SerializeField] GameObject cat_2;
-    [SerializeField] GameObject cat_3;
+    [SerializeField] List<GameObject> cats;
+    [SerializeField] GameObject fish;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +16,21 @@ public class CatSelector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(GameObject cat in cats)
+        {
+            if(isInRadius(cat.transform.position, fish.transform.position, 3))
+            {
+                cat.GetComponent<agent>().enabled = true;
+                break;
+            }
+        }
+    }
+
+    bool isInRadius(Vector3 pos_a, Vector3 pos_b, float radius)
+    {
+        if ((pos_b.x - pos_a.x) * (pos_b.x - pos_a.x) + (pos_b.z - pos_a.z) * (pos_b.z - pos_a.z) <= radius * radius)
+            return true;
+
+        return false;
     }
 }

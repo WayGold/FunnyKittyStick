5921a0f6429086d75524682f08d47c5a8ba56ed8
Wei Zeng <wzeng@ucsd.edu>
2022-03-31 12:03:40 -0600

    cat selector

---
+++ Assets/Scripts/Wei_dev/CatSelector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSelector : MonoBehaviour
+{
+    [SerializeField] GameObject cat_1;
+    [SerializeField] GameObject cat_2;
+    [SerializeField] GameObject cat_3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

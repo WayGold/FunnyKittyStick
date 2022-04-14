9603c46f1f887b491043bb2b174a162ef233aa8b
https://gapp-git.eng.utah.edu/u1306392 <u1306392.umail.Utah.edu>
2022-04-14 00:04:00 -0600

    Wiimote tracking implemented

--- Assets/Scripts/Han_dev/Joycon/JoyconDemo.cs
+++ Assets/Scripts/Han_dev/Joycon/JoyconDemo.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -111,3 +111,4 @@ namespace JoyConLibrary
 		}
 	}
 }
+*/
--- Assets/Scripts/Jared_dev/StickTrackerWiimote.cs
+++ Assets/Scripts/Jared_dev/StickTrackerWiimote.cs
@@ -16,7 +16,9 @@ public class StickTrackerWiimote : MonoBehaviour
     private float resetOffsetHeartbeatTimeInSeconds = 0.2f;
 
     private Vector3 originalStickPosition;
-    private float maxXOffset = 30f;
+    private Vector3 offsetVector = Vector3.zero;
+    private float xBounds = 8f;
+    private float yBounds = 6f;
     private float maxYOffset = 20f;
     private float maxZOffset = 17f;
 
@@ -26,9 +28,13 @@ public class StickTrackerWiimote : MonoBehaviour
 
     private float minDotDistance = 0.15f;
     private float maxDotDistance = 0.65f;
-    private float sensitivity = 1.0f;
+    private float sensitivity = 0.5f;
 
-    public float rayDistance = 10f;
+    private float dotDistance = 0.0f;
+
+    public float rayDistance = 70f;
+
+    private bool gyroMode = false;
 
     // Start is called before the first frame update
     IEnumerator Start()
@@ -53,16 +59,19 @@ public class StickTrackerWiimote : MonoBehaviour
 
         this.wiimote = WiimoteManager.Wiimotes[0];
 
+        yield return new WaitForSeconds(2.0f);
+
         //Set up gyroscope
-        wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_IR10_EXT9);
+        this.wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
 
         this.wiimote.RequestIdentifyWiiMotionPlus();
         wiimote.ReadWiimoteData();
-        
+
         while (this.wiimote.wmp_attached == false)
         {
             Debug.LogError("Initializing WMP...");
             yield return null;
+            this.wiimote = WiimoteManager.Wiimotes[0];
             this.wiimote.RequestIdentifyWiiMotionPlus();
             wiimote.ReadWiimoteData();
         }
@@ -78,83 +87,139 @@ public class StickTrackerWiimote : MonoBehaviour
         //stickObject.transform.rotation = Quaternion.FromToRotation(stickObject.transform.rotation.eulerAngles, Vector3.up) * stickObject.transform.rotation;
         //stickObject.transform.rotation = Quaternion.FromToRotation(stickObject.transform.forward, Vector3.forward) * stickObject.transform.rotation;
 
+        this.wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_IR12);
+        this.wiimote.SetupIRCamera(IRDataType.BASIC);
+
         StartCoroutine(ResetOffsetHeartbeat());
+        StartCoroutine(TrackStick());
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.CenterToCamera();
+        if (!WiimoteManager.HasWiimote() || this.wiimote.wmp_attached == false)
+        {
+            Debug.Log("AAAHHHHH");
+            return;
+        }
         
         int ret;
         do
         {
-            if (this.wiimote == null)
-            {
-                return;
-            }
-
             this.wiimote = WiimoteManager.Wiimotes[0];
 
             ret = this.wiimote.ReadWiimoteData();
 
             if (wiimote.current_ext == ExtensionController.MOTIONPLUS && wiimote.Button.b)
             {
-                Vector3 offset = new Vector3(wiimote.MotionPlus.PitchSpeed,
-                                                -wiimote.MotionPlus.YawSpeed,
-                                                wiimote.MotionPlus.RollSpeed) / 95f; // Divide by 95Hz (average updates per second from wiimote)
+                if (this.gyroMode == false)
+                {
+                    this.gyroMode = true;
+                    this.wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
+                }
+
+                //Debug.LogError("TADA: " + wiimote.MotionPlus.PitchSpeed + " " + wiimote.MotionPlus.YawSpeed + " " + wiimote.MotionPlus.RollSpeed);
+
+                Vector3 offset = new Vector3(wiimote.MotionPlus.PitchSpeed * this.sensitivity,
+                                                -wiimote.MotionPlus.YawSpeed * this.sensitivity,
+                                                wiimote.MotionPlus.RollSpeed * this.sensitivity) / 95f; // Divide by 95Hz (average updates per second from wiimote)
                 this.wmpOffset += offset;
 
                 this.stickObject.transform.Rotate(this.wmpOffset, Space.Self);
             }
             else
             {
-                Vector3 offsetVector = Vector3.zero;
-
+                if (this.gyroMode == true)
+                {
+                    this.gyroMode = false;
+                    this.wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_IR12);
+                    this.wiimote.SetupIRCamera(IRDataType.BASIC);
+                }
+                
+                //this.wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_IR12);
                 //For Tracking X and Y positioning
-                float[] pointer = wiimote.Ir.GetPointingPosition();
+                /*float[] pointer = wiimote.Ir.GetPointingPosition();
 
                 float xOffset = pointer[0] * this.sensitivity * this.maxXOffset;
                 offsetVector = new Vector3(offsetVector.x - xOffset, offsetVector.y, offsetVector.z - xOffset);
 
                 float yOffset = pointer[1] * this.sensitivity * this.maxYOffset;
                 offsetVector = new Vector3(offsetVector.x, offsetVector.y - yOffset, offsetVector.z);
+                */
 
                 //For tracking Z positioning
-                Vector2[] sensorDots = new Vector2[2];
-                float[,] irData = wiimote.Ir.GetProbableSensorBarIR();
-                for (int i = 0; i < 2; i++)
-                {
-                    float normalizedX = (float)irData[i, 0] / 1023f;
-                    float normalizedY = (float)irData[i, 1] / 767f;
 
-                    if (normalizedX != -1 && normalizedY != -1)
-                    {
-                        sensorDots[i] = new Vector2(normalizedX, normalizedY);
-                    }
-                }
-
-                float dotDistance = Vector2.Distance(sensorDots[0], sensorDots[1]);
-                float zOffset = 0.0f;
-
-                if (dotDistance > 0.0f)
-                {
-                    zOffset = (dotDistance / this.maxDotDistance) * this.sensitivity * this.maxZOffset;
-
-                    offsetVector = new Vector3(offsetVector.x - zOffset, offsetVector.y, offsetVector.z + zOffset);
-
-                    this.stickHolder.transform.position = this.originalStickPosition + offsetVector;
-
-                    //this.stickHolder.transform.position = Vector3.Lerp(this.stickHolder.transform.position,
-                       // this.stickHolder.transform.position + offsetVector, this.sensitivity);
-                }
             }      
         } while (ret > 0);
     }
 
-    private void SetXYPosition()
-    { 
-        
+    private void SetXYZPosition()
+    {
+        float[] pointer = this.wiimote.Ir.GetPointingPosition();
+
+        //Debug.LogError("IR Pointing Position: " + pointer[0]);
+
+        //X Position
+        //This ain't perfect, but it's what I got
+        //Maps 0 - 1 to -1 - 1
+        float sanitizedValue = pointer[0];
+        if (sanitizedValue > 1.0f)
+        {
+            sanitizedValue = 1.0f;
+        }
+        else if (sanitizedValue < 0.0f)
+        {
+            sanitizedValue = 0.0f;
+        }
+
+        float xPointer = (sanitizedValue * 2) - 1.0f;
+
+        float xOffset = xPointer * this.xBounds;
+
+        //Y Position
+        sanitizedValue = pointer[1];
+        if (sanitizedValue > 1.0f)
+        {
+            sanitizedValue = 1.0f;
+        }
+        else if (sanitizedValue < 0.0f)
+        {
+            sanitizedValue = 0.0f;
+        }
+
+        float yPointer = (sanitizedValue * 2) - 1.0f;
+
+        float yOffset = yPointer * this.yBounds;
+
+        //Z Position
+        Vector2[] sensorDots = new Vector2[2];
+        float[,] irData = wiimote.Ir.GetProbableSensorBarIR();
+        for (int i = 0; i < 2; i++)
+        {
+            float normalizedX = (float)irData[i, 0] / 1023f;
+            float normalizedY = (float)irData[i, 1] / 767f;
+
+            if (normalizedX != -1 && normalizedY != -1)
+            {
+                sensorDots[i] = new Vector2(normalizedX, normalizedY);
+            }
+        }
+
+        this.dotDistance = Vector2.Distance(sensorDots[0], sensorDots[1]);
+        float zOffset = 0.0f;
+
+        if (this.dotDistance > 0.0f)
+        {
+            zOffset = (((this.dotDistance / this.maxDotDistance) * 2) - 1) * this.maxZOffset;
+
+            //offsetVector = new Vector3(offsetVector.x - zOffset, offsetVector.y, offsetVector.z + zOffset);
+        }
+
+
+        if ((pointer[0] > 0.0f && pointer[0] < 1.0f) && (pointer[1] > 0.0f && pointer[1] < 1.0f))
+        {
+            this.offsetVector = new Vector3(xOffset - zOffset, yOffset, xOffset + zOffset);
+        }
     }
 
     private void CenterToCamera()
@@ -171,5 +236,30 @@ public class StickTrackerWiimote : MonoBehaviour
             this.wmpOffset = Vector3.zero;
             yield return null;
         }
+    }
+
+    private IEnumerator TrackStick()
+    {
+        while (!WiimoteManager.HasWiimote()) { yield return null; }
+
+        while (true)
+        {
+            this.CenterToCamera();
+          
+            if (wiimote.Button.b == false)
+            {
+                this.SetXYZPosition();             
+            }
+
+            this.stickHolder.transform.position += this.offsetVector;
+
+            yield return null;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        this.wiimote = null;
+        //WiimoteManager.Cleanup(WiimoteManager.Wiimotes[0]);
     }
 }

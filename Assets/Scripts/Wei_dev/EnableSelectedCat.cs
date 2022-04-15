363d48d5890ac8a4d9806cb0914b300fbea21bc5
Wei Zeng <wzeng@ucsd.edu>
2022-04-15 15:18:03 -0600

    select cat update, connect to main

---
+++ Assets/Scripts/Wei_dev/EnableSelectedCat.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSelectedCat : MonoBehaviour
+{
+    [SerializeField] public GameObject cat_white;
+    [SerializeField] public GameObject cat_short;
+    [SerializeField] public GameObject cat_grey;
+
+    private bool first_entrance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        first_entrance = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!first_entrance)
+            return;
+
+        switch (CatSelection.CAT_SELECTED)
+        {
+            case 0:
+                cat_grey.SetActive(true);
+                break;
+            case 1:
+                cat_short.SetActive(true);
+                break;
+            case 2:
+                cat_white.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+        first_entrance = false;
+    }
+}
